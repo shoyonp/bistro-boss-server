5b65cba67342080473eb46294790d5516ebb72bd
shoyonp <shoyonkumar143@gmail.com>
2025-01-11 00:57:36 +0600

    item delete functionality done

--- index.js
+++ index.js
@@ -135,10 +135,35 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
+    });
+
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      // console.log(item);
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        },
+      };
+      const resule = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(resule)
     });
 
     app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
