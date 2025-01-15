0357e2beb15a5274e5953b62d3399c1f81e7afcb
shoyonp <shoyonkumar143@gmail.com>
2025-01-15 16:31:42 +0600

    cart clear done

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
     // jwt related apis
     app.post("/jwt", async (req, res) => {
@@ -213,6 +214,22 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item form the cart
+      // const query = {_id:new ObjectId(id)}
+      // console.log("payment info", payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({paymentResult,deleteResult});
     });
 
     // Send a ping to confirm a successful connection
